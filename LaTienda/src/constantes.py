5c32edd451fe57b5011d28d40a725ad1f7292a86
daferarte <daferarte@gmail.com>
2024-10-03 07:35:51 -0500

    AGREGANDO enum

---
+++ LaTienda/src/constantes.py
@@ -0,0 +1,12 @@
+__author__ = "Daniel Fernando Arteaga"
+__license__ = "GPL"
+__version__ = "1.0.0"
+__email__ = "daniel.arteagafajar@campusucc.edu.co"
+
+"""----------------------------------------------------------------
+# Constantes
+----------------------------------------------------------------"""
+
+IVA_PAPELERIA = 0.10
+IVA_SUPERMERCADO = 0.04
+IVA_FARMACIA = 0.12
