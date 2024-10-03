b4ef9c71ab08428df3704bf33dd09b47975efff9
daferarte <daferarte@gmail.com>
2024-10-03 08:20:32 -0500

    agregando metodos

--- LaTienda/src/Producto.py
+++ LaTienda/src/Producto.py
@@ -4,6 +4,7 @@ __version__ = "1.0.0"
 __email__ = "daniel.arteagafajar@campusucc.edu.co"
 
 import constantes
+from Tipos import Tipo
 
 class Producto:
     
@@ -18,27 +19,81 @@ class Producto:
     """----------------------------------------------------------------
     # Constructor
     ----------------------------------------------------------------"""
-    def __init__(self, valorUnitario:float, subsidiado: bool, calidad):
+    __method__ = "Constructor"
+    __parameter__ = "Ninguno"
+    __returns__ = "Ninguna"
+    __Description__ = "metodo constructor de la clase"
+    def __init__(self, nombre, tipo, valorUnitario:float, cantidad:int, cantidadMinima:int, subsidiado: bool, calidad):
         
         """----------------------------------------------------------------
         # Atributos
         ----------------------------------------------------------------"""
-        
-        self.valorUnitario = valorUnitario
-        self.subsidiado = subsidiado
-        self.calidad = calidad
+        self.__nombre=nombre
+        self.__tipo=tipo
+        self.__valorUnitario = valorUnitario
+        self.__subsidiado = subsidiado
+        self.__calidad = calidad
+        self.__cantidadBodega = cantidad
+        self.__cantidadMinima = cantidadMinima
+        self.__cantidadUnidadesVendidas = 0
         """----------------------------------------------------------------
         # self.subsidiado = True 
         # self.subsidiado = False
         ----------------------------------------------------------------"""
+        # tipo = Tipo.DOGUERIA
     
     """----------------------------------------------------------------
     # Metodos
     ----------------------------------------------------------------"""
     
+    __method__ = "CalcularPrecioPapeleria"
+    __parameter__ = "Ninguno"
+    __returns__ = "Ninguna"
+    __Description__ = "metodo que calcula el precio de un producto tipo papeleria"
     def CalcularPrecioPapeleria(self):
         
         # self.precio = self.valorUnitario + (self.valorUnitario * self.IVA_PAPELERIA)
         self.precio = self.valorUnitario + (self.valorUnitario * constantes.IVA_PAPELERIA)
         
+    __method__ = "DarNombre"
+    __parameter__ = "Ninguno"
+    __returns__ = "Nombre del producto"
+    __Description__ = "metodo que retorna el nombre del producto"
+    def DarNombre(self):
+        return self.__nombre
+    
+    __method__ = "DarTIpo"
+    __parameter__ = "Ninguno"
+    __returns__ = "Tipo del producto"
+    __Description__ = "Retorna el tipo del producto"
+    def DarTIpo(self):
+        return self.__tipo
+    
+    __method__ = "DarValorUnitario"
+    __parameter__ = "Ninguno"
+    __returns__ = "Valor Unitario"
+    __Description__ = "Retorna el Valor Unitario"
+    def DarValorUnitario(self):
+        return self.__valorUnitario
+    
+    __method__ = "DarCantidadBodega"
+    __parameter__ = "Ninguno"
+    __returns__ = "Cantidad Bodega"
+    __Description__ = "Retorna la cantidad en bodega"
+    def DarCantidadBodega(self):
+        return self.__cantidadBodega
+    
+    __method__ = "DarCantidadMinima"
+    __parameter__ = "Ninguno"
+    __returns__ = "Cantidad Minima"
+    __Description__ = "Retorna la cantidad minima en bodega"
+    def DarCantidadMinima(self):
+        return self.__cantidadMinima
         
+    __method__ = "DarCantidadUnidadesVendidas"
+    __parameter__ = "Ninguno"
+    __returns__ = "CantidadUnidadesVendidas"
+    __Description__ = "Retorna la cantidad de unidades vendidas"
+    def DarCantidadUnidadesVendidas(self):
+        return self.__cantidadUnidadesVendidas
+    
--- LaTienda/src/Tienda.py
+++ LaTienda/src/Tienda.py
@@ -2,3 +2,61 @@ __author__ = "Daniel Fernando Arteaga"
 __license__ = "GPL"
 __version__ = "1.0.0"
 __email__ = "daniel.arteagafajar@campusucc.edu.co"
+
+class Tienda:
+    
+    """----------------------------------------------------------------
+    # Constructor
+    ----------------------------------------------------------------"""
+    __method__ = "Constructor"
+    __parameter__ = "Ninguno"
+    __returns__ = "Ninguna"
+    __Description__ = "metodo constructor de la clase"
+    def __init__(self):
+        
+        """----------------------------------------------------------------
+        # Atributos
+        ----------------------------------------------------------------"""
+        self.__producto1 = None
+        self.__producto2 = None
+        self.__producto3 = None
+        self.__producto4 = None 
+        self.__dineroCaja:float = 0.0
+        
+    """----------------------------------------------------------------
+    # Metodos
+    ----------------------------------------------------------------"""
+    __method__ = "DarProducto1"
+    __parameter__ = "Ninguno"
+    __returns__ = "Producto1"
+    __Description__ = "metodo que retorna la informacion de producto 1"
+    def DarProducto1(self):
+        return self.__producto1
+    
+    __method__ = "DarProducto2"
+    __parameter__ = "Ninguno"
+    __returns__ = "Producto2"
+    __Description__ = "metodo que retorna la informacion de producto 2"
+    def DarProducto2(self):
+        return self.__producto2
+    
+    __method__ = "DarProducto3"
+    __parameter__ = "Ninguno"
+    __returns__ = "Producto3"
+    __Description__ = "metodo que retorna la informacion de producto 3"
+    def DarProducto3(self):
+        return self.__producto3
+    
+    __method__ = "DarProducto4"
+    __parameter__ = "Ninguno"
+    __returns__ = "Producto4"
+    __Description__ = "metodo que retorna la informacion de producto 4"
+    def DarProducto4(self):
+        return self.__producto4
+    
+    __method__ = "DarDineroCaja"
+    __parameter__ = "Ninguno"
+    __returns__ = "Dinero en caja"
+    __Description__ = "metodo que retorna el dinero en caja"
+    def DarDineroCaja(self):
+        return self.__dineroCaja
